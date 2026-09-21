2bbdcb1a040562c43734dd15347fc2d2eaca86f5
jorgemiguel7462@gmail.com <jorgemiguel7462@gmail.com>
2026-09-20 22:48:04 -0300

    feat: listagem de eventos

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
 import Header from "./components/Header";
+import EventoCard from "./components/EventoCard";
+import { eventos } from "./data/eventos";
 import "./App.css";
 
 function App() {
-  const curso = 'Engenharia de Software'
-
   return (
     <>
       <Header />
-      <div className="app">
-        <p>Portal de eventos acadêmicos — {curso}</p>
-        <p>Em construção, aula a aula, até a banca final.</p>
-      </div>
+      <main className="lista-eventos">
+        {eventos.map((evento) => (
+          <EventoCard
+            key={evento.id}
+            titulo={evento.titulo}
+            tipo={evento.tipo}
+            data={evento.data}
+            local={evento.local}
+            vagas={evento.vagas}
+          />
+        ))}
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
