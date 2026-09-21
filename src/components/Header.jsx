d5a6086a2c6d011601d8c354f84b3d728c23515b
jorgemiguel7462@gmail.com <jorgemiguel7462@gmail.com>
2026-09-20 22:12:08 -0300

    feat: dados estaticos de eventos

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header className="app-header">
+      <h1>UBM Eventos</h1>
+      <p>Portal de eventos acadêmicos do Centro Universitário de Barra Mansa</p>
+    </header>
+  );
+}
+
+export default Header;
